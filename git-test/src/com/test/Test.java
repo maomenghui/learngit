f5cb82b785deba5c18a9842b63a09f83f87d8467
maomao <563596610@qq.com>
2018-07-09 13:41:32 +0800

    :wq

--- git-test/src/com/test/Test.java
+++ git-test/src/com/test/Test.java
@@ -1,7 +1,15 @@
 package com.test;
 
+import java.util.Arrays;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int[] array = {2,8,5,4};
+		System.out.println(binarySearch(array, 5));
+	}
+	
+	public static int binarySearch(int[] array, int key){
+		return Arrays.binarySearch(array, key);
 	}
 }
